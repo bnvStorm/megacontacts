076fdefff87a02087777049d441f8990c08a91b7
bnvStorm <39797487+bnvStorm@users.noreply.github.com>
2020-12-13 20:20:12 +0600

    main upd

--- includes/head.php
+++ includes/head.php
@@ -8,8 +8,8 @@
  <!-- social -->
 <meta property="og:title" content="Промотход Сервис">
 <meta property="og:type" content="article">
-<meta property="og:url" content="https://test.4920.kz/">
-<meta property="og:image" content="https://test.4920.kz/style/images/social.jpg">
+<meta property="og:url" content="https://rpo.kz/">
+<meta property="og:image" content="https://rpo.kz/style/images/social.jpg">
 <meta property="og:site_name" content="Промотход Сервис">
 <!-- <meta property="fb:admins" content="">  -->
 <meta property="og:description" content="Экологичные контейнерные площадки для вашего бизнеса">
@@ -19,8 +19,8 @@
 <!-- <meta name="twitter:site" content="@Freepik_Vectors">
 <meta name="twitter:creator" content="@Freepik_Vectors"> -->
 <meta name="twitter:title" content="Промотход Сервис">
-<meta name="twitter:image:src" content="https://test.4920.kz/style/images/social.jpg">
-<meta name="twitter:url" content="https://test.4920.kz/">
+<meta name="twitter:image:src" content="https://rpo.kz/style/images/social.jpg">
+<meta name="twitter:url" content="https://rpo.kz/">
 <meta name="twitter:description" content="Экологичные контейнерные площадки для вашего бизнеса">
   <!-- social -->
 <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
@@ -29,3 +29,7 @@
       <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
       <![endif]-->
 <link rel="stylesheet" href="../nstyle.css">
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script src="../js/jquery.magnific-popup.js"></script>
+<script src="../js/main.js"></script>
+    <link rel="stylesheet" href="../css/magnific-popup.css">
