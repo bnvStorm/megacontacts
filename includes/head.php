83e3e6180ed9ef225e82cc0f216cd6ad6b6befbc
bnvStorm <39797487+bnvStorm@users.noreply.github.com>
2020-12-13 21:38:55 +0600

    Update head.php

--- includes/head.php
+++ includes/head.php
@@ -6,22 +6,22 @@
 <link rel="shortcut icon" href="../300.ico">
 
  <!-- social -->
-<meta property="og:title" content="Промотход Сервис">
+<!-- <meta property="og:title" content="Промотход Сервис">
 <meta property="og:type" content="article">
-<meta property="og:url" content="https://rpo.kz/">
-<meta property="og:image" content="https://rpo.kz/style/images/social.jpg">
-<meta property="og:site_name" content="Промотход Сервис">
+<meta property="og:url" content="https://rpo.kz/"> -->
+<!-- <meta property="og:image" content="https://rpo.kz/style/images/social.jpg"> -->
+<!-- <meta property="og:site_name" content="Промотход Сервис"> -->
 <!-- <meta property="fb:admins" content="">  -->
-<meta property="og:description" content="Экологичные контейнерные площадки для вашего бизнеса">
+<!-- <meta property="og:description" content="Экологичные контейнерные площадки для вашего бизнеса"> -->
 <meta property="og:locale" content="ru_RU">
 
-<meta name="twitter:card" content="summary_large_image">
+<!-- <meta name="twitter:card" content="summary_large_image"> -->
 <!-- <meta name="twitter:site" content="@Freepik_Vectors">
 <meta name="twitter:creator" content="@Freepik_Vectors"> -->
-<meta name="twitter:title" content="Промотход Сервис">
+<!-- <meta name="twitter:title" content="Промотход Сервис">
 <meta name="twitter:image:src" content="https://rpo.kz/style/images/social.jpg">
-<meta name="twitter:url" content="https://rpo.kz/">
-<meta name="twitter:description" content="Экологичные контейнерные площадки для вашего бизнеса">
+<meta name="twitter:url" content="https://rpo.kz/"> -->
+<!-- <meta name="twitter:description" content="Экологичные контейнерные площадки для вашего бизнеса"> -->
   <!-- social -->
 <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
 <!--[if lt IE 9]>
